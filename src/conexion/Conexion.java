e292c432fc000a802f67e5fab2933d67662d187d
gomezreyes35 <gomezreyes35@gmail.com>
2023-11-03 13:45:41 +0100

    Clase conexion

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -1,5 +1,19 @@
 package conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 //Clase conexion para conectar a BBDD hay muchos ejemplos para conectar java con xampp sera solo copiar
 public class Conexion {
+    Connection conectar=null;
+    public Connection conexion(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conectar= DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba", "root", "");
+            System.out.println("Se ha conectado");
+        } catch (Exception e){
+            System.out.println("No se ha podido conectar" +" "+ e.getMessage());
+        }
+        return conectar;
+    }
 }
